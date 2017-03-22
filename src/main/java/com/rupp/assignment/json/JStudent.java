75ddda74aaf178774627337c12c6412b71fd162c
Sophal Ngann <ngannsophal12@gmail.com>
2017-03-23 06:53:15 +0700

    student

---
+++ src/main/java/com/rupp/assignment/json/JStudent.java
@@ -0,0 +1,19 @@
+package com.rupp.assignment.json;
+
+import java.sql.Date;
+
+public class JStudent extends JCommon  {
+    /** Unique id for this Entity in the database */
+	
+    private String firstname;
+    private String lastname;    
+    private String sex;
+    private String dob;
+    
+    private String address;
+    private String contact_person_name;
+    private String contact_person_relationship;    
+    private String contact_person_phone;
+	
+   
+}
